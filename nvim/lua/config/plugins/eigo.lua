bd601a7d94573bfae8d6d48349ff4a514d0cce00
wisteriahuman <wisteriahuman91@gmail.com>
2026-07-20 21:37:23 +0900

    nvim: skip eigo.nvim

--- nvim/lua/config/plugins/eigo.lua
+++ nvim/lua/config/plugins/eigo.lua
@@ -1,7 +1,10 @@
+local eigo_dir = vim.fn.expand("~/Projects/eigo/eigo.nvim")
+
 return {
   {
-    dir = vim.fn.expand("~/Projects/eigo/eigo.nvim"),
+    dir = eigo_dir,
     name = "eigo.nvim",
+    cond = vim.uv.fs_stat(eigo_dir) ~= nil,
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("eigo").setup()
